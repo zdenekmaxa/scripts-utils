56d5a378360713976c2560edf2334defaee5e0b3
Zdenek Maxa <zdenekmaxa@yahoo.co.uk>
2015-01-30 14:45:10 +0100

    added support to read datetime from EXIF

--- ratt/ratt.py
+++ ratt/ratt.py
@@ -13,6 +13,9 @@ target file names are counter suffixed.
 File extension is preserved.
 The time in the final file names can be changed by hour offset argument.
 
+Could read the time information either from file's modification time or
+from files EXIF information.
+
 Use case:
 Naming photo files from a digital cammera according to date/time name pattern.
 Shooting somewhere in different timezone while forgetting to change time on
@@ -40,19 +43,32 @@ DAYS_OF_WEEK = ("mon", "tue", "wed", "thu", "fri", "sat", "sun")
 import sys
 import os
 import time
+import datetime
 from functools import partial
 from optparse import OptionParser
-from datetime import datetime
-from datetime import timedelta
+
+import exifread
 
 
 def process_input_args(inputArgs):
     parser = OptionParser()
-    parser.add_option("-c", "--confirm", action="store_true", dest="confirm",
+    parser.add_option("-c",
+                      "--confirm",
+                      action="store_true",
+                      dest="confirm",
                       default=False,
                       help="Confirmation to eventually rename files.")
-    parser.add_option("-o", "--offset_hour", dest="offset_hour", default=0,
+    parser.add_option("-o",
+                      "--offset_hour",
+                      dest="offset_hour",
+                      default=0,
                       help="Modify time in files names by +/- 1 .. 11 hours.")
+    parser.add_option("-e",
+                      "--exif",
+                      action="store_true",
+                      dest="exif",
+                      default=False,
+                      help="Read data from EXIF info from files.")
     options, args = parser.parse_args(inputArgs)
     # sanitize - offset has to be integer
     try:
@@ -61,7 +77,7 @@ def process_input_args(inputArgs):
         print("Wrong offset: '%s', setting back default." %
               options.offset_hour)
         offset_hour = 0
-    return options.confirm, offset_hour
+    return options.confirm, offset_hour, options.exif
 
 
 def get_date_time_string(timestamp, hour_offset):
@@ -71,9 +87,9 @@ def get_date_time_string(timestamp, hour_offset):
     shifted by +/- offset if defined (if non-zero).
 
     """
-    dt = datetime.fromtimestamp(timestamp)
+    dt = datetime.datetime.fromtimestamp(timestamp)
     if hour_offset:
-        dt += timedelta(hours=hour_offset)
+        dt += datetime.timedelta(hours=hour_offset)
     tt = dt.timetuple()
     fnDict = dict(year="%d" % tt[0],
                   month="%02d" % tt[1],
@@ -86,12 +102,32 @@ def get_date_time_string(timestamp, hour_offset):
     return fn
 
 
-def get_new_file_names(curr_file_names, hour_offset):
+def get_timestamp_from_exif(file_name):
+    # format of data as read from exif
+    dt_format = "%Y:%m:%d %H:%M:%S"
+    fd = open(file_name, "rb")
+    data = exifread.process_file(fd, strict=True, stop_tag="DateTimeOriginal")
+    # returned in a form '2014:06:28 11:53:21'
+    dt = str(data["EXIF DateTimeOriginal"])
+    fd.close()
+    # timestamp representation of the datetime
+    ts = time.mktime(datetime.datetime.strptime(dt, dt_format).timetuple())
+    # check by converting back via timestamp
+    check_dt = datetime.datetime.fromtimestamp(ts).strftime(dt_format)
+    assert dt == check_dt
+    return ts
+
+
+def get_new_file_names(curr_file_names, hour_offset, exif):
     for name in curr_file_names:
         if os.path.isdir(name):
             continue
-        # get the file's last modification date
-        timestamp = os.path.getmtime(name)
+        if exif:
+            # get the file date and time from EXIF
+            timestamp = get_timestamp_from_exif(name)
+        else:
+            # get the file's last modification date
+            timestamp = os.path.getmtime(name)
         date_time_file_name = get_date_time_string(timestamp, hour_offset)
         yield date_time_file_name, name
 
@@ -105,9 +141,9 @@ def do_rename(old_name, new_name, confirmation):
         os.rename(old_name, new_name)
 
 
-def get_file_rename_data(data_source_func, hour_offset):
+def get_file_rename_data(data_source_func, hour_offset, exif):
     # file name items
-    fn_items = get_new_file_names(data_source_func(), hour_offset)
+    fn_items = get_new_file_names(data_source_func(), hour_offset, exif)
     # key = destination filename (still without counter in case of duplicates)
     # value = [list of old names], number of items determines suffix counter
     data = {}
@@ -134,9 +170,9 @@ def rename_according_to_time_and_date(data, confirmation):
 
 
 def main():
-    confirmation, hour_offset = process_input_args(sys.argv[1:])
+    confirmation, hour_offset, exif = process_input_args(sys.argv[1:])
     data_source_func = partial(os.listdir, os.getcwd())
-    data = get_file_rename_data(data_source_func, hour_offset)
+    data = get_file_rename_data(data_source_func, hour_offset, exif)
     rename_according_to_time_and_date(data, confirmation)
     if not confirmation:
         print("\n\nrun with '-c' to really perform files renaming.\n")
